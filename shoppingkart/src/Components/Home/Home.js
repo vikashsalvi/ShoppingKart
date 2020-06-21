433f3b10d7ef1a99bbe690c292c741573d71dbe7
Deep Muni <deep.muni94@gmail.com>
2020-06-20 23:56:08 -0300

    Landing page modified

--- shoppingkart/src/Components/Home/Home.js
+++ shoppingkart/src/Components/Home/Home.js
@@ -1,11 +1,17 @@
 import React, {Component} from "react";
 import Brand from "../Brands/Brands";
+import AboutUs from "../AboutUs/AboutUs";
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <div>NAVBAR</div>
+                <div>CAROUSEL</div>
+                <div>PRODUCTS</div>
                 <Brand/>
+                <AboutUs/>
+                <div>FOOTER</div>
             </div>
         );
     }
