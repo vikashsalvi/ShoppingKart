ac891f611e156fcc3c745d027ff8401ccfc00057
Deep Muni <deep.muni94@gmail.com>
2020-06-20 23:42:08 -0300

    Brands added on landing page

--- shoppingkart/src/Components/Home/Home.js
+++ shoppingkart/src/Components/Home/Home.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
+import Brand from "../Brands/Brands";
 
 class Home extends Component {
     render() {
         return (
-            <div>Home page</div>
+            <div>
+                <Brand/>
+            </div>
         );
     }
 }
-
- 
 
 export default Home;
