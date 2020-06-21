44b7f8c2eb03da614dab3047ec4a366115b59fd5
Deep Muni <deep.muni94@gmail.com>
2020-06-20 23:22:01 -0300

    Profile page added

--- shoppingkart/src/Components/Route/Routes.js
+++ shoppingkart/src/Components/Route/Routes.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "../Home/Home";
+import Profile from "../UserProfile/UserProfile";
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route exact path='/profile' component={Profile}/>
             </Switch>
         );
     }
