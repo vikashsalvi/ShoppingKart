d47dff8458500409bf7fba4771eaebb67d882827
Deep Muni <deep.muni94@gmail.com>
2020-06-20 23:32:52 -0300

    Register Page added

--- shoppingkart/src/Components/Route/Routes.js
+++ shoppingkart/src/Components/Route/Routes.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "../Home/Home";
+import Register from "../Signup/Signup";
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route exact path='/register' component={Register}/>
             </Switch>
         );
     }
