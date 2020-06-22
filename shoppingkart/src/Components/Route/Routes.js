e25b459d5a7ffd162a457b8bc492b1dff67a5fa0
pallavi <pl561741@dal.ca>
2020-06-22 20:00:51 -0300

    Cart management functionality

--- shoppingkart/src/Components/Route/Routes.js
+++ shoppingkart/src/Components/Route/Routes.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "../Home/Home";
+import Cart from "../CartManagement/Mycart";
+import OrderConfirmation from "../CartManagement/Checkout/Checkout";
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route exact path="/mycart" component={Cart}/>
+                <Route exact path='/orderConfirmation' component={OrderConfirmation}/>
             </Switch>
         );
     }
