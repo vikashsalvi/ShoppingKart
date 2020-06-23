c586866b67fa83d3a4eee63291f44c497fb1ffee
rashmika <rashmika.ibrahimpatnam@dal.ca>
2020-06-22 18:17:23 -0700

    adding admin product creation code

--- shoppingkart/src/Components/Route/Routes.js
+++ shoppingkart/src/Components/Route/Routes.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "../Home/Home";
+import Register from "../Signup/Signup";
+import Login from "../Login/Login";
+import CreateProduct from '../Admin/Admin';
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route exact path='/register' component={Register}/>
+                <Route exact path= '/login' component={Login}/>
+                <Route exact path='/createProduct' component={CreateProduct}/>
             </Switch>
         );
     }
