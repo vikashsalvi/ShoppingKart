eb21ec2fad80faf40c36d795948d76c3966403d9
Deep Muni <deep.muni94@gmail.com>
2020-07-20 12:12:17 -0300

    Assignment 4 (Deep Muni): Added CORS settings

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const productRoute = require('./api/route/productRoute');
 const mongoose = require('mongoose');
+
+app.use(cors());
 
 const url = "mongodb+srv://deepmuni:MPrfJkmBJDOYvXQK@testing-deep-aets3.mongodb.net/CSCI_5709_Project?retryWrites=true&w=majority";
 
@@ -23,9 +26,6 @@ app.use(express.static(__dirname + '/shoppingkart/build/'));
 
 app.get('/',function(req,res, next){
     res.sendFile(path.join(__dirname + '/shoppingkart/build/index.html'));
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
