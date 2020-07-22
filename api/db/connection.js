51ea8a917c7a7fb2793156272568e058b7aa3422
Vikash Salvi <vikash.salvi@dal.ca>
2020-07-22 11:09:10 -0300

    Update connection.js
    Assignment 4(Vikash Salvi): Connection changes

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://webUser:admin123@cluster0.jb5qq.mongodb.net/test";
+const url = "mongodb+srv://webUser:admin123@cluster0.jb5qq.mongodb.net/CSCI_5709_Project?retryWrites=true&w=majority";
 
 mongoose.connect(url,{
     useNewUrlParser: true,
