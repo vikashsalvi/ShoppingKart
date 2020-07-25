1153f5669d69890e3cc20fa4410f916b6ca682c2
Bharat16 <bharatbhargava9@gmail.com>
2020-07-25 17:47:41 -0300

    Assignment 4:(Bharat Bhargava) final changes for product review functionality

--- api/controller/productReviewController.js
+++ api/controller/productReviewController.js
@@ -31,10 +31,10 @@ const postProductreview = (req, res) => {
 /**
  @function author    Vikash Salvi => B00838074
  **/
-const getProductReview = (req,res) => {
-    productReviewModel.find({"productId" : req.params.query}).exec()
+const getProductReview = (req, res) => {
+    productReviewModel.find({ "productId": req.params.query }).exec()
         .then(data => {
-            res.json({ Status :"Success", data : data});
+            res.json({ Status: "Success", data: data });
         })
         .catch(err => {
             console.log("Failure:" + err);
