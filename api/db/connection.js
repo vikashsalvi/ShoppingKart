5e5b7c4095b904efa074fa276b8aef914907887c
Vikash Salvi <vikash.salvi@dal.ca>
2020-08-21 13:01:20 -0300

    Update connection.js

--- api/db/connection.js
+++ api/db/connection.js
@@ -7,7 +7,7 @@
 const mongoose = require('mongoose');
 
 
-const url = "mongodb+srv://webUser:admin123@cluster0.jb5qq.mongodb.net/CSCI_5709_Project?retryWrites=true&w=majority"
+const url = "WRITE_MONGO_URL"
 
 mongoose.connect(url,{
     useNewUrlParser: true,
